30ebeeeb65742d42924460c4875ecaaadb48e0c0
André Krebs <andre.krebssilva@gmail.com>
2019-02-16 13:57:35 -0200

    Configuração inicial do back-end com spring e add arquivo de configuração do docker para base de dados mysql

---
+++ pessoas-backend/src/main/java/com/andre/app/PessoasBackendApplication.java
@@ -0,0 +1,14 @@
+package com.andre.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoasBackendApplication.class, args);
+	}
+
+}
+
